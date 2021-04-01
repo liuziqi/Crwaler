53b1f0756172183aa282a8d12e2cdfcf15b76afc
xiaomi <lzq17@icloud.com>
2021-04-01 21:03:18 +0800

    爬取药监总局化妆品公司数据

--- 06.爬取药监总局相关数据.py
+++ 06.爬取药监总局相关数据.py
@@ -1,14 +1,44 @@
 import requests
 
 if __name__ == "__main__":
-    url = "http://scxk.nmpa.gov.cn:81/xk/"
-
+    # 爬取前10页的公司ID
+    url = "http://scxk.nmpa.gov.cn:81/xk/itownet/portalAction.do?method=getXkzsList"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36 Edg/89.0.774.50"
     }
 
-    page_text = requests.get(url=url, headers=headers).text
+    ID = []
 
-    with open("./yaojian.html", "w", encoding="utf-8") as fp:
-        fp.write(page_text)
+    for i in range(10):
+        params = {
+            "on": "true",
+            "page": str(i+1),
+            "pageSize": "15",
+            "productName": "",
+            "conditionType": "1",
+            "applyname": "",
+            "applysn": ""
+        }
+        page_json = requests.post(url=url, headers=headers, params=params).json()
+
+        for j in range(len(page_json["list"])):
+            ID.append(page_json["list"][j]["ID"])
+
+    # 根据id爬取公司信息
+    url = "http://scxk.nmpa.gov.cn:81/xk/itownet/portalAction.do?method=getXkzsById"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36 Edg/89.0.774.50"
+    }
+
+    info = []
+
+    for id in ID:
+        params = {
+            "id": id
+        }
+        page_json = requests.post(url=url, headers=headers, params=params).json()
+        info.append(page_json)
+
+    print(info)
+    print(len(info))
 
